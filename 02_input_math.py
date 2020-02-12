223c3ae615300be613cc50d604ffde4c4eadf04c
bellc709191 <60583538+bellc709191@users.noreply.github.com>
2020-02-13 11:54:14 +1300

    Create 02_input_math.py

---
+++ 02_input_math.py
@@ -0,0 +1,14 @@
+# get input
+
+# ask user for name
+name = input("What is your name?")
+
+# ask user for two numbers
+
+# add numbers together
+
+# multiply numbers together
+
+# greet user and display math
+print ("Hello", name)
+
